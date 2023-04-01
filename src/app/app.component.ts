e7efd350bd7c4e26ba7300fa9c32b6059b27a207
Michael Zabka <zabka.michael@gmail.com>
2023-04-01 15:28:21 +0000

    Add sos.onReady() for Angular app
    the rest of sos js api can be called after onReady is resolved.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import sos from '@signageos/front-applet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-app';
+  public sosIsReady: boolean = false;
+
+  public ngOnInit(): void {
+    this.initSos();
+  }
+
+  private initSos(): void {
+    sos.onReady().then(() => {
+      console.log('Ready to rock');
+      this.sosIsReady = true;
+    });
+  }
 }
